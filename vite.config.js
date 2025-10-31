352b6a85acc9328c1e43cbb04c0a50d7c5c2089c
cutty <148048291+cutty92@users.noreply.github.com>
2025-10-31 15:05:19 +0100

    vercel testing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This ensures assets are loaded relative to index.html
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 })
